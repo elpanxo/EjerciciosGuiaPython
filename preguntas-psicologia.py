8a319ec79bee519a04596111c04c73ca78dcd6c8
elpanxo <166082166+elpanxo@users.noreply.github.com>
2024-05-05 00:26:38 -0400

    Add files via upload

---
+++ preguntas-psicologia.py
@@ -0,0 +1,121 @@
+print("1. ¿La psicología es el estudio científico de la mente y el comportamiento?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+puntaje = 0
+
+if respuesta == "a":
+    puntaje = 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("2. ¿Sigmund Freud fue el fundador del psicoanálisis?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("3. ¿La depresión es exclusivamente causada por factores genéticos?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "b":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("4. ¿La teoría del apego de John Bowlby se centra en la importancia de las relaciones tempranas con los cuidadores?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("5. ¿El trastorno obsesivo-compulsivo (TOC) es exclusivamente causado por el estrés?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "b":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("6. ¿La psicología gestalt se enfoca en entender la totalidad de la experiencia perceptual?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("7. ¿El psicoanálisis se basa principalmente en técnicas de modificación del comportamiento?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "b":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("8. ¿La psicología cognitiva estudia principalmente cómo procesamos, almacenamos y utilizamos la información?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("9. ¿El trastorno de personalidad antisocial es más común en mujeres que en hombres?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "b":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("10.¿La terapia cognitivo-conductual (TCC) se centra en cambiar los patrones de pensamiento y comportamiento disfuncionales?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("11. ¿La psicología humanista se centra principalmente en la influencia del inconsciente en el comportamiento humano?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "b":
+    puntaje += 1.0 
+
+print("Tu puntaje es:", puntaje)
+
+print("12. ¿El trastorno de estrés postraumático (TEPT) puede desarrollarse como resultado de experiencias traumáticas?")
+print("a. Si")
+print("b. No")
+respuesta = input("Ingresa la letra correspondiente a tu respuesta: ")
+
+if respuesta == "a":
+    puntaje += 1.0 
+
+print("Tu puntaje total es:", puntaje)
